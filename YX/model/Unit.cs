c9c614c51599a1923e384bdbe39d7e6b9dfbda29
yangpeng.na <yangpeng.na@outlook.com>
2025-12-20 13:41:44 +0800

    vv

---
+++ YX/model/Unit.cs
@@ -0,0 +1,15 @@
+﻿namespace YX.model
+{
+    public enum TorqueUnit
+    {
+        Nm,
+        mNm,
+        Kgcm,
+        gcm
+    }
+    public enum CurrentUnit
+    {
+        A,
+        mA
+    }
+}
